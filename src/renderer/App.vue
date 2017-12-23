7d379106219cc1f33e773ef3bfba3c8cb7b930eb
jon20 <kawadu1221@gmail.com>
2017-12-23 16:05:12 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,49 @@
+<template>
+<v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+</v-navigation-drawer>
+<v-toolbar color="indigo" dark fixed app>
+  <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+<v-toolbar-title>RssReader</v-toolbar-title>
+</v-toolbar>
+    <router-view></router-view>
+</v-app>
+</template>
+
+<script>
+  export default {
+    name: 'rss',
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/Next.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-link to="/">Success!</router-link>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: 'hello-page'
+  }
+</script>
