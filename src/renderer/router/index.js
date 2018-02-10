3b558e23e035bb17a63c880a697ea5c4e27a2086
jon20 <kawadu1221@gmail.com>
2018-02-10 16:19:02 +0900

    modify routing error

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/rss',
+      path: '/home',
       name: 'rss',
       component: require('@/components/Rss').default
     },
